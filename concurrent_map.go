72fe7c56fa764fa57aea6fbc3794d847e21e6e68
Alodi <magimaki@yeah.net>
2023-02-13 13:07:55 +0800

    feat(db): add concurrent map

--- concurrent_map.go
+++ concurrent_map.go
@@ -1,16 +1,132 @@
 package lazydb
 
 import (
+	"hash/fnv"
 	"sync"
 )
 
-const shardCount = 32 // or 64
+const (
+	defaultShardCount = 16
+)
 
-type MapShard[K comparable, V any] struct {
-	simpleMap map[K]V
-	mu        sync.RWMutex
+type MapShard[K comparable] struct {
+	simpleMap    map[K]any
+	sync.RWMutex // r&w lock for every shard
 }
 
-type ConcurrentMap[K comparable, V any] struct {
-	shards []*MapShard[K, V]
+// Get gets the value under a given key.
+func (ms *MapShard[K]) Get(key K) (any, bool) {
+	val, ok := ms.simpleMap[key]
+	return val, ok
+}
+
+// Set sets the key and value under a specific MapShard.
+func (ms *MapShard[K]) Set(key K, value any) {
+	ms.simpleMap[key] = value
+
+}
+
+// Has returns if the map contains a specific key.
+func (ms *MapShard[K]) Has(key K) bool {
+	_, ok := ms.simpleMap[key]
+	return ok
+}
+
+// Remove deletes an element from the map.
+func (ms *MapShard[K]) Remove(key K) {
+	delete(ms.simpleMap, key)
+}
+
+// Pop deletes an element from the map and returns it.
+func (ms *MapShard[K]) Pop(key K) (any, bool) {
+	val, exist := ms.simpleMap[key]
+	delete(ms.simpleMap, key)
+	return val, exist
+}
+
+type ConcurrentMap[K comparable] struct {
+	shards     []*MapShard[K]
+	sharding   func(key K) uint32
+	shardCount uint8
+}
+
+// NewConcurrentMap returns a ConcurrentMap[string] with string keys by default.
+func NewConcurrentMap(mapShardCount int) *ConcurrentMap[string] {
+	// fnv32 function only supports string keys
+	cm := newConcurrentMap[string](mapShardCount, fnv32)
+	return &cm
+}
+
+func newConcurrentMap[K comparable](mapShardCount int, sharding func(key K) uint32) ConcurrentMap[K] {
+	// suggest powers of 2
+	if mapShardCount < defaultShardCount {
+		mapShardCount = defaultShardCount
+	}
+
+	cm := ConcurrentMap[K]{
+		sharding:   sharding,
+		shards:     make([]*MapShard[K], mapShardCount),
+		shardCount: uint8(mapShardCount),
+	}
+
+	for i := 0; i < mapShardCount; i++ {
+		cm.shards[i] = &MapShard[K]{simpleMap: make(map[K]any)}
+	}
+
+	return cm
+}
+
+func fnv32(key string) uint32 {
+	h := fnv.New32()
+	_, _ = h.Write([]byte(key))
+	return h.Sum32()
+}
+
+// GetShard returns the MapShard under the given key.
+func (cm *ConcurrentMap[K]) GetShard(key K) *MapShard[K] {
+	return cm.shards[uint(cm.sharding(key))%uint(cm.shardCount)]
+}
+
+// GetShardByReading returns the MapShard under the given key after RLocking.
+func (cm *ConcurrentMap[K]) GetShardByReading(key K) *MapShard[K] {
+	shard := cm.GetShard(key)
+	shard.RLock()
+	return shard
+}
+
+// GetShardByWriting returns the MapShard under the given key after Locking.
+func (cm *ConcurrentMap[K]) GetShardByWriting(key K) *MapShard[K] {
+	shard := cm.GetShard(key)
+	shard.Lock()
+	return shard
+}
+
+// Get gets the value under a given key.
+func (cm *ConcurrentMap[K]) Get(key K) (any, bool) {
+	shard := cm.GetShard(key)
+	return shard.Get(key)
+}
+
+// Set sets the key and value under a specific MapShard.
+func (cm *ConcurrentMap[K]) Set(key K, value any) {
+	shard := cm.GetShard(key)
+	shard.Set(key, value)
+}
+
+// Has returns if the map contains a specific key.
+func (cm *ConcurrentMap[K]) Has(key K) bool {
+	shard := cm.GetShard(key)
+	return shard.Has(key)
+}
+
+// Remove deletes an element from the map.
+func (cm *ConcurrentMap[K]) Remove(key K) {
+	shard := cm.GetShard(key)
+	shard.Remove(key)
+}
+
+// Pop deletes an element from the map and returns it.
+func (cm *ConcurrentMap[K]) Pop(key K) (any, bool) {
+	shard := cm.GetShard(key)
+	return shard.Pop(key)
 }
---
+++ concurrent_map_test.go
@@ -0,0 +1,262 @@
+package lazydb
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMapShard_Get(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable] struct {
+		name      string
+		ms        *MapShard[K]
+		args      args[K]
+		valueWant any
+		flagWant  bool
+	}
+	tests := []testCase[string]{
+		{
+			name: "test1",
+			ms: &MapShard[string]{
+				simpleMap: map[string]any{
+					Name: "test1",
+				},
+			},
+			args: args[string]{
+				key: Name,
+			},
+			valueWant: "test1",
+			flagWant:  true,
+		},
+		{
+			name: "test2",
+			ms: &MapShard[string]{
+				simpleMap: make(map[string]any),
+			},
+			args: args[string]{
+				key: Name,
+			},
+			valueWant: nil,
+			flagWant:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valueGot, flagGot := tt.ms.Get(tt.args.key)
+			if !reflect.DeepEqual(valueGot, tt.valueWant) {
+				t.Errorf("Get() valueGot = %v, valueWant %v", valueGot, tt.valueWant)
+			}
+			if flagGot != tt.flagWant {
+				t.Errorf("Get() flagGot = %v, flagWant %v", flagGot, tt.flagWant)
+			}
+		})
+	}
+}
+
+func TestMapShard_Set(t *testing.T) {
+	type args[K comparable] struct {
+		key   K
+		value any
+	}
+	type testCase[K comparable] struct {
+		name string
+		ms   *MapShard[K]
+		args args[K]
+	}
+	tests := []testCase[string]{
+		{
+			name: "test1",
+			ms: &MapShard[string]{
+				simpleMap: make(map[string]any),
+			},
+			args: args[string]{
+				key:   Name,
+				value: "test1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.ms.Set(tt.args.key, tt.args.value)
+			valueGot, flagGot := tt.ms.Get(tt.args.key)
+			if !reflect.DeepEqual(valueGot, tt.args.value) {
+				t.Errorf("Get() valueGot = %v, valueWant %v", valueGot, tt.args.value)
+			}
+			if flagGot != true {
+				t.Errorf("Get() flagGot = %v, flagWant %v", flagGot, true)
+			}
+		})
+	}
+}
+
+func TestMapShard_Has(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable] struct {
+		name string
+		ms   *MapShard[K]
+		args args[K]
+		want bool
+	}
+	tests := []testCase[string]{
+		{
+			name: "test1",
+			ms: &MapShard[string]{
+				simpleMap: map[string]any{
+					Name: "test1",
+				},
+			},
+			args: args[string]{
+				key: Name,
+			},
+			want: true,
+		},
+		{
+			name: "test2",
+			ms: &MapShard[string]{
+				simpleMap: make(map[string]any),
+			},
+			args: args[string]{
+				key: Name,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.ms.Has(tt.args.key); got != tt.want {
+				t.Errorf("Has() = %v, valueWant %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapShard_Remove(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable] struct {
+		name string
+		ms   *MapShard[K]
+		args args[K]
+	}
+	tests := []testCase[string]{
+		{
+			name: "test1",
+			ms: &MapShard[string]{
+				simpleMap: map[string]any{
+					Name: "test1",
+				},
+			},
+			args: args[string]{
+				key: Name,
+			},
+		},
+		{
+			name: "test2",
+			ms: &MapShard[string]{
+				simpleMap: make(map[string]any),
+			},
+			args: args[string]{
+				key: Name,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.ms.Remove(tt.args.key)
+			valueGot, flagGot := tt.ms.Get(tt.args.key)
+			if !reflect.DeepEqual(valueGot, nil) {
+				t.Errorf("Get() valueGot = %v, valueWant %v", valueGot, nil)
+			}
+			if flagGot != false {
+				t.Errorf("Get() flagGot = %v, flagWant %v", flagGot, false)
+			}
+		})
+	}
+}
+
+func TestMapShard_Pop(t *testing.T) {
+	type args[K comparable] struct {
+		key K
+	}
+	type testCase[K comparable] struct {
+		name      string
+		ms        *MapShard[K]
+		args      args[K]
+		valueWant any
+		flagWant  bool
+	}
+	tests := []testCase[string]{
+		{
+			name: "test1",
+			ms: &MapShard[string]{
+				simpleMap: map[string]any{
+					Name: "test1",
+				},
+			},
+			args: args[string]{
+				key: Name,
+			},
+			valueWant: "test1",
+			flagWant:  true,
+		},
+		{
+			name: "test2",
+			ms: &MapShard[string]{
+				simpleMap: make(map[string]any),
+			},
+			args: args[string]{
+				key: Name,
+			},
+			valueWant: nil,
+			flagWant:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valueGot, flagGot := tt.ms.Pop(tt.args.key)
+			if !reflect.DeepEqual(valueGot, tt.valueWant) {
+				t.Errorf("Pop() valueGot = %v, valueWant %v", valueGot, tt.valueWant)
+			}
+			if flagGot != tt.flagWant {
+				t.Errorf("Pop() flagGot = %v, flagWant %v", flagGot, tt.flagWant)
+			}
+			valueGot, flagGot = tt.ms.Get(tt.args.key)
+			if !reflect.DeepEqual(valueGot, nil) {
+				t.Errorf("Get() valueGot = %v, valueWant %v", valueGot, nil)
+			}
+			if flagGot != false {
+				t.Errorf("Get() flagGot = %v, flagWant %v", flagGot, false)
+			}
+		})
+	}
+}
+
+func TestNewConcurrentMap(t *testing.T) {
+	type args struct {
+		mapShardCount int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "test1",
+			args: args{
+				mapShardCount: -4,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewConcurrentMap(tt.args.mapShardCount)
+			if len(got.shards) != defaultShardCount {
+				t.Errorf("ShardCount Got = %v, Want %v", got.shardCount, defaultShardCount)
+			}
+		})
+	}
+}
