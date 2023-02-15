bb2966510bee3f1cf9fadd0bf2f0b62a82eb86b7
billsjc123 <1034341626@qq.com>
2023-02-15 21:16:58 +0800

    feat(db): add config for merge interval and iotype

--- config.go
+++ config.go
@@ -1,6 +1,31 @@
 package lazydb
 
+import (
+	"lazydb/logfile"
+	"time"
+)
+
+const (
+	defaultMaxLogFileSize       int64          = 512 << 20
+	defaultLogFileMergeInterval time.Duration  = time.Hour * 8
+	defaultIOType               logfile.IOType = logfile.FileIO
+)
+
 type DBConfig struct {
-	DBPath         string
-	MaxLogFileSize int64
+	DBPath               string        // Directory path for storing log files on disk.
+	MaxLogFileSize       int64         // Max capacity of a log file.
+	LogFileMergeInterval time.Duration // Max time interval for merging log files.
+
+	//  IOType
+	//  Only support FileIO at the moment
+	IOType logfile.IOType
+}
+
+func DefaultDBConfig(path string) *DBConfig {
+	return &DBConfig{
+		DBPath:               path,
+		MaxLogFileSize:       defaultMaxLogFileSize,
+		LogFileMergeInterval: defaultLogFileMergeInterval,
+		IOType:               defaultIOType,
+	}
 }
