3ac91bab01fbf3fac1d7189a651b1670a5ae991e
billsjc123 <1034341626@qq.com>
2023-02-15 20:25:16 +0800

    feat(db): add interfaces of db

--- db.go
+++ db.go
@@ -1,14 +1,42 @@
 package lazydb
 
 import (
+	"lazydb/logfile"
 	"sync"
 )
 
-type LazyDB struct {
-	cfg   *DBConfig
-	index *ConcurrentMap[string]
-	mu    sync.RWMutex
-}
+type (
+	LazyDB struct {
+		cfg             *DBConfig
+		index           *ConcurrentMap[string]
+		fidsMap         *ConcurrentMap[valueType]         // [valueType][]uint32
+		curLogFile      *ConcurrentMap[valueType]         // [valueType]*LogFile
+		archivedLogFile map[valueType]*ConcurrentMap[int] // [int]*LogFile
+		mu              sync.RWMutex
+	}
+
+	valueType uint8
+
+	Value struct {
+		value     []byte    // 这个是他的memory mode用的，它支持和redis一样的内存读数据
+		vType     valueType // 可以加个type 用于type指令和一些操作前的判断，比如string的key不能lpush
+		fid       uint32
+		offset    int64
+		entrySize int
+		expiredAt int64
+	}
+
+	// 写LogFile之后返回位置信息的结构体
+	ValuePos struct {
+		fid       uint32
+		offset    int64
+		entrySize int
+	}
+)
+
+const (
+	valueTypeString valueType = iota
+)
 
 func Open() (*LazyDB, error) {
 	return nil, nil
@@ -19,5 +47,25 @@ func (db *LazyDB) Sync() error {
 }
 
 func (db *LazyDB) Close() error {
+	return nil
+}
+
+func (db *LazyDB) Merge(typ valueType, targetFid uint32) error {
+	return nil
+}
+
+func (db *LazyDB) readLogEntry(typ valueType, fid uint32, offset int64) (*logfile.LogEntry, error) {
+	return nil, nil
+}
+
+func (db *LazyDB) writeLogEntry(typ valueType, entry *logfile.LogEntry) (*ValuePos, error) {
+	return nil, nil
+}
+
+func (db *LazyDB) initLogFiles(typ valueType) error {
+	return nil
+}
+
+func (db *LazyDB) buildLogFiles() error {
 	return nil
 }
