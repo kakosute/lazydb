ba23bfd23afe6318c5fc48fadce7831837296882
billsjc123 <1034341626@qq.com>
2023-02-15 21:51:05 +0800

    feat(db): add util functions and interfaces

--- db.go
+++ db.go
@@ -1,7 +1,9 @@
 package lazydb
 
 import (
+	"errors"
 	"lazydb/logfile"
+	"log"
 	"sync"
 )
 
@@ -9,17 +11,17 @@ type (
 	LazyDB struct {
 		cfg             *DBConfig
 		index           *ConcurrentMap[string]
-		fidsMap         *ConcurrentMap[valueType]         // [valueType][]uint32
-		curLogFile      *ConcurrentMap[valueType]         // [valueType]*LogFile
-		archivedLogFile map[valueType]*ConcurrentMap[int] // [int]*LogFile
+		fidsMap         *ConcurrentMap[valueType]            // [valueType][]uint32
+		curLogFile      *ConcurrentMap[valueType]            // [valueType]*LogFile
+		archivedLogFile map[valueType]*ConcurrentMap[uint32] // [uint32]*LogFile
 		mu              sync.RWMutex
 	}
 
 	valueType uint8
 
 	Value struct {
-		value     []byte    // 这个是他的memory mode用的，它支持和redis一样的内存读数据
-		vType     valueType // 可以加个type 用于type指令和一些操作前的判断，比如string的key不能lpush
+		value     []byte
+		vType     valueType
 		fid       uint32
 		offset    int64
 		entrySize int
@@ -38,6 +40,11 @@ const (
 	valueTypeString valueType = iota
 )
 
+var (
+	ErrLogFileNotExist = errors.New("Log file is not exist")
+	ErrOpenLogFile     = errors.New("Open Log file error")
+)
+
 func Open() (*LazyDB, error) {
 	return nil, nil
 }
@@ -54,11 +61,37 @@ func (db *LazyDB) Merge(typ valueType, targetFid uint32) error {
 	return nil
 }
 
-func (db *LazyDB) readLogEntry(typ valueType, fid uint32, offset int64) (*logfile.LogEntry, error) {
+func (db *LazyDB) getValue(key []byte) (*Value, error) {
 	return nil, nil
 }
 
+// readLogEntry Reads entry from log files by fid and offset.
+// Return error if entry does not exist.
+func (db *LazyDB) readLogEntry(typ valueType, fid uint32, offset int64) (*logfile.LogEntry, error) {
+	var lf *logfile.LogFile
+
+	curLogFile := db.getCurLogFile(typ)
+	if curLogFile == nil {
+		return nil, ErrOpenLogFile
+	}
+
+	if curLogFile.Fid == fid {
+		lf = curLogFile
+	} else {
+		lf = db.getArchivedLogFile(typ, fid)
+		if lf == nil {
+			return nil, ErrLogFileNotExist
+		}
+	}
+
+	entry, _, err := lf.ReadLogEntry(offset)
+	return entry, err
+}
+
+// writeLogEntry writes entry into active log file and returns position.
+// Return nil and error if writing fails.
 func (db *LazyDB) writeLogEntry(typ valueType, entry *logfile.LogEntry) (*ValuePos, error) {
+
 	return nil, nil
 }
 
@@ -66,6 +99,43 @@ func (db *LazyDB) initLogFiles(typ valueType) error {
 	return nil
 }
 
+// buildLogFiles Recover archivedLogFile from disk.
 func (db *LazyDB) buildLogFiles() error {
+	return nil
+}
+
+// getCurLogFile Util function for get curLogFile from ConcurrentMap.
+// Initiate a new LogFile if curLogFile of typ is empty
+func (db *LazyDB) getCurLogFile(typ valueType) *logfile.LogFile {
+	v, ok := db.curLogFile.Get(typ)
+	// create a new LogFile
+	if !ok {
+		//todo param typ
+		lf, err := logfile.Open(db.cfg.DBPath, 1, db.cfg.MaxLogFileSize, typ, db.cfg.IOType)
+		if err != nil {
+			log.Fatalf("Create log file error: %v", err)
+			return nil
+		}
+
+		db.curLogFile.Set(typ, lf)
+		return lf
+	}
+	lf, _ := v.(*logfile.LogFile)
+	return lf
+}
+
+// getArchivedLogFile Util function for get archivedLogFile from ConcurrentMap.
+// Returns nil when target log file does not exist
+func (db *LazyDB) getArchivedLogFile(typ valueType, fid uint32) *logfile.LogFile {
+	lfs := db.archivedLogFile[typ]
+	v, ok := lfs.Get(fid)
+	if !ok {
+		return nil
+	}
+	lf := v.(*logfile.LogFile)
+	return lf
+}
+
+func (db *LazyDB) getFidListByType(typ valueType) []uint32 {
 	return nil
 }
