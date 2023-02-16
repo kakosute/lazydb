ee19ee2bc3c03434f4b0e36f49bece6d3a0a8f68
Boomchick <1227674944@qq.com>
2023-02-16 17:00:43 +0800

    iocontroller

--- iocontroller/fileio.go
+++ iocontroller/fileio.go
@@ -1,1 +1,72 @@
 package iocontroller
+
+import (
+	"errors"
+	"os"
+)
+
+// FilePerm default permission of the newly created log file.
+const FilePerm = 0644
+
+// ErrInvalidFsize invalid file size.
+var ErrInvalidFsize = errors.New("fsize can`t be zero or negative")
+
+// FileIOController represents using standard file I/O.
+type FileIOController struct {
+	fd *os.File // system file descriptor.
+}
+
+// NewFileIOController creates a new file io selector.
+func NewFileIOController(fName string, fsize int64) (IOController, error) {
+	if fsize <= 0 {
+		return nil, ErrInvalidFsize
+	}
+	file, err := openFile(fName, fsize)
+	if err != nil {
+		return nil, err
+	}
+	return &FileIOController{fd: file}, nil
+}
+
+func (f *FileIOController) Write(b []byte, offset int64) (int, error) {
+	return f.fd.WriteAt(b, offset)
+}
+
+func (f *FileIOController) Read(b []byte, offset int64) (int, error) {
+	return f.fd.ReadAt(b, offset)
+}
+
+func (f *FileIOController) Sync() error {
+	return f.fd.Sync()
+}
+
+func (f *FileIOController) Close() error {
+	return f.fd.Close()
+}
+
+func (f *FileIOController) Delete() error {
+	if err := f.fd.Close(); err != nil {
+		return err
+	}
+	return os.Remove(f.fd.Name())
+}
+
+// open file and truncate it if necessary.
+func openFile(fName string, fsize int64) (*os.File, error) {
+	fd, err := os.OpenFile(fName, os.O_CREATE|os.O_RDWR, FilePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	stat, err := fd.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if stat.Size() < fsize {
+		if err := fd.Truncate(fsize); err != nil {
+			return nil, err
+		}
+	}
+	return fd, nil
+}
---
+++ iocontroller/io_controller_test.go
@@ -0,0 +1,313 @@
+package iocontroller
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestNewFileIOSelector(t *testing.T) {
+	testNewIOController(t, 0)
+}
+
+func TestFileIOController_Write(t *testing.T) {
+	testIOControllerWrite(t, 0)
+}
+
+func TestFileIOController_Read(t *testing.T) {
+	testIOControllerRead(t, 0)
+}
+
+func TestFileIOController_Sync(t *testing.T) {
+	testIOControllerSync(t, 0)
+}
+
+func TestFileIOController_Close(t *testing.T) {
+	testIOControllerClose(t, 0)
+}
+
+func TestFileIOController_Delete(t *testing.T) {
+	testIOControllerDelete(t, 0)
+}
+
+func testNewIOController(t *testing.T, ioType uint8) {
+	type args struct {
+		fName string
+		fsize int64
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"size-zero", args{fName: "000000001.wal", fsize: 0},
+		},
+		{
+			"size-negative", args{fName: "000000002.wal", fsize: -1},
+		},
+		{
+			"size-big", args{fName: "000000003.wal", fsize: 1024 << 20},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			absPath, err := filepath.Abs(filepath.Join("/tmp", tt.args.fName))
+			assert.Nil(t, err)
+			var ioController IOController
+			if ioType == 0 {
+				ioController, err = NewFileIOController(absPath, tt.args.fsize)
+			}
+
+			defer func() {
+				if ioController != nil {
+					err = ioController.Delete()
+					assert.Nil(t, err)
+				}
+			}()
+			if tt.args.fsize > 0 {
+				assert.Nil(t, err)
+				assert.NotNil(t, ioController)
+			} else {
+				assert.Equal(t, err, ErrInvalidFsize)
+			}
+		})
+	}
+}
+
+func testIOControllerWrite(t *testing.T, ioType uint8) {
+	absPath, err := filepath.Abs(filepath.Join("/tmp", "00000001.vlog"))
+	assert.Nil(t, err)
+	var size int64 = 1048576
+
+	var ioController IOController
+	if ioType == 0 {
+		ioController, err = NewFileIOController(absPath, size)
+	}
+
+	assert.Nil(t, err)
+	defer func() {
+		if ioController != nil {
+			_ = ioController.Delete()
+		}
+	}()
+
+	type fields struct {
+		ioController IOController
+	}
+	type args struct {
+		b      []byte
+		offset int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			"nil-byte", fields{ioController: ioController}, args{b: nil, offset: 0}, 0, false,
+		},
+		{
+			"one-byte", fields{ioController: ioController}, args{b: []byte("0"), offset: 0}, 1, false,
+		},
+		{
+			"many-bytes", fields{ioController: ioController}, args{b: []byte("lazydb"), offset: 0}, 6, false,
+		},
+		{
+			"bigvalue-byte", fields{ioController: ioController}, args{b: []byte(fmt.Sprintf("%01048576d", 123)), offset: 0}, 1048576, false,
+		},
+		{
+			"exceed-size", fields{ioController: ioController}, args{b: []byte(fmt.Sprintf("%01048577d", 123)), offset: 0}, 1048577, false,
+		},
+		{
+			"EOF-error", fields{ioController: ioController}, args{b: []byte("lazydb"), offset: -1}, 0, true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.fields.ioController.Write(tt.args.b, tt.args.offset)
+			// io.EOF err in mmmap.
+			if tt.want == 1048577 && ioType == 1 {
+				tt.wantErr = true
+				tt.want = 0
+			}
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Write() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Write() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func testIOControllerRead(t *testing.T, ioType uint8) {
+	absPath, err := filepath.Abs(filepath.Join("/tmp", "00000001.wal"))
+	var ioController IOController
+	if ioType == 0 {
+		ioController, err = NewFileIOController(absPath, 100)
+	}
+
+	assert.Nil(t, err)
+	defer func() {
+		if ioController != nil {
+			_ = ioController.Delete()
+		}
+	}()
+	offsets := writeSomeData(ioController, t)
+	results := [][]byte{
+		[]byte(""),
+		[]byte("1"),
+		[]byte("lazydb"),
+	}
+
+	type fields struct {
+		ioController IOController
+	}
+	type args struct {
+		b      []byte
+		offset int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			"nil", fields{ioController: ioController}, args{b: make([]byte, 0), offset: offsets[0]}, 0, false,
+		},
+		{
+			"one-byte", fields{ioController: ioController}, args{b: make([]byte, 1), offset: offsets[1]}, 1, false,
+		},
+		{
+			"many-bytes", fields{ioController: ioController}, args{b: make([]byte, 6), offset: offsets[2]}, 6, false,
+		},
+		{
+			"EOF-1", fields{ioController: ioController}, args{b: make([]byte, 100), offset: -1}, 0, true,
+		},
+		{
+			"EOF-2", fields{ioController: ioController}, args{b: make([]byte, 100), offset: 1024}, 0, true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.fields.ioController.Read(tt.args.b, tt.args.offset)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Read() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Read() got = %v, want %v", got, tt.want)
+			}
+			if !tt.wantErr {
+				assert.Equal(t, tt.args.b, results[i])
+			}
+		})
+	}
+}
+
+func writeSomeData(ioController IOController, t *testing.T) []int64 {
+	tests := [][]byte{
+		[]byte(""),
+		[]byte("1"),
+		[]byte("lazydb"),
+	}
+
+	var offsets []int64
+	var offset int64
+	for _, tt := range tests {
+		offsets = append(offsets, offset)
+		n, err := ioController.Write(tt, offset)
+		assert.Nil(t, err)
+		offset += int64(n)
+	}
+	return offsets
+}
+
+func testIOControllerSync(t *testing.T, ioType uint8) {
+	sync := func(id int, fsize int64) {
+		absPath, err := filepath.Abs(filepath.Join("/tmp", fmt.Sprintf("0000000%d.wal", id)))
+		assert.Nil(t, err)
+		var ioController IOController
+		if ioType == 0 {
+			ioController, err = NewFileIOController(absPath, fsize)
+		}
+
+		assert.Nil(t, err)
+		defer func() {
+			if ioController != nil {
+				_ = ioController.Delete()
+			}
+		}()
+		writeSomeData(ioController, t)
+		err = ioController.Sync()
+		assert.Nil(t, err)
+	}
+
+	for i := 1; i < 4; i++ {
+		sync(i, int64(i*100))
+	}
+}
+
+func testIOControllerClose(t *testing.T, ioType uint8) {
+	sync := func(id int, fsize int64) {
+		absPath, err := filepath.Abs(filepath.Join("/tmp", fmt.Sprintf("0000000%d.wal", id)))
+		defer func() {
+			_ = os.Remove(absPath)
+		}()
+		assert.Nil(t, err)
+		var ioController IOController
+		if ioType == 0 {
+			ioController, err = NewFileIOController(absPath, fsize)
+		}
+
+		assert.Nil(t, err)
+		defer func() {
+			if ioController != nil {
+				err := ioController.Close()
+				assert.Nil(t, err)
+			}
+		}()
+		writeSomeData(ioController, t)
+		assert.Nil(t, err)
+	}
+
+	for i := 1; i < 4; i++ {
+		sync(i, int64(i*100))
+	}
+}
+
+func testIOControllerDelete(t *testing.T, ioType uint8) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"1", false},
+		{"2", false},
+		{"3", false},
+		{"4", false},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			absPath, err := filepath.Abs(filepath.Join("/tmp", fmt.Sprintf("0000000%d.wal", i)))
+			assert.Nil(t, err)
+			var ioController IOController
+			if ioType == 0 {
+				ioController, err = NewFileIOController(absPath, int64((i+1)*100))
+			}
+
+			assert.Nil(t, err)
+
+			if err := ioController.Delete(); (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
