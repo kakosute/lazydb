7e040013d7d64027168280f24b54785604bd9f27
Alodi <magimaki@yeah.net>
2023-03-06 11:23:56 +0800

    fix(index): simplified buildIndexFromLogFiles testing

--- index_test.go
+++ index_test.go
@@ -28,15 +28,12 @@ func TestLazyDB_buildIndexFromLogFiles(t *testing.T) {
 		activeLogFileMap: make(map[valueType]*MutexLogFile),
 		archivedLogFile:  make(map[valueType]*ConcurrentMap[uint32]),
 	}
+	defer destroyDB(db)
+
 	for i := 0; i < logFileTypeNum; i++ {
 		db.fidsMap[valueType(i)] = &MutexFids{fids: make([]uint32, 0)}
 		db.archivedLogFile[valueType(i)] = NewWithCustomShardingFunction[uint32](defaultShardCount, simpleSharding)
 	}
-
-	// test buildLogFiles with empty directory
-	err := db.buildLogFiles()
-	assert.Nil(t, err)
-	assert.Equal(t, uint32(1), db.getActiveLogFile(valueTypeString).lf.Fid)
 
 	val1 := GetValue32()
 	val2 := GetValue32()
@@ -44,39 +41,19 @@ func TestLazyDB_buildIndexFromLogFiles(t *testing.T) {
 	db.writeLogEntry(valueTypeString, &logfile.LogEntry{Key: GetKey(1), Value: val1})
 	db.writeLogEntry(valueTypeString, &logfile.LogEntry{Key: GetKey(2), Value: val2})
 	db.writeLogEntry(valueTypeString, &logfile.LogEntry{Key: GetKey(3), Value: val3})
-	db.Close()
 
-	// test buildLogFiles with existing log files
-	newDB := &LazyDB{
-		cfg:              &cfg,
-		index:            NewConcurrentMap(int(cfg.HashIndexShardCount)),
-		fidsMap:          make(map[valueType]*MutexFids),
-		activeLogFileMap: make(map[valueType]*MutexLogFile),
-		archivedLogFile:  make(map[valueType]*ConcurrentMap[uint32]),
-	}
-	defer destroyDB(newDB)
-
-	for i := 0; i < logFileTypeNum; i++ {
-		newDB.fidsMap[valueType(i)] = &MutexFids{fids: make([]uint32, 0)}
-		newDB.archivedLogFile[valueType(i)] = NewWithCustomShardingFunction[uint32](defaultShardCount, simpleSharding)
-	}
-	err = newDB.buildLogFiles()
-	assert.NoError(t, err)
-	assert.Equal(t, uint32(2), newDB.getActiveLogFile(valueTypeString).lf.Fid)
-	assert.NotNil(t, newDB.getArchivedLogFile(valueTypeString, 1))
-
-	err = newDB.buildIndexFromLogFiles()
+	err := db.buildIndexFromLogFiles()
 	assert.NoError(t, err)
 
-	got, err := newDB.Get(GetKey(1))
+	got, err := db.Get(GetKey(1))
 	assert.NoError(t, err)
 	assert.Equal(t, true, reflect.DeepEqual(got, val1))
 
-	got, err = newDB.Get(GetKey(2))
+	got, err = db.Get(GetKey(2))
 	assert.NoError(t, err)
 	assert.Equal(t, true, reflect.DeepEqual(got, val2))
 
-	got, err = newDB.Get(GetKey(3))
+	got, err = db.Get(GetKey(3))
 	assert.NoError(t, err)
 	assert.Equal(t, true, reflect.DeepEqual(got, val3))
 }
